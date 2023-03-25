6edd4836efd3765608dbd0cdb762fab33f6c26f5
Mo2024 <mohdosama2025@gmail.com>
2023-03-25 23:25:12 +0300

    fixed more routes

--- controllers/profile.inc.php
+++ controllers/profile.inc.php
@@ -5,7 +5,7 @@ if(isset($_SESSION['userId']) && !empty($_SESSION['userId'])){
     } 
 }else{
     $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    setcookie("redirect", $url,);
+    setcookie("redirect", $url,0,'/');
     header("Location: /ITCS333-Project/auth/signin.php");
     
 }
