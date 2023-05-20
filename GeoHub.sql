1bf9234d0da00e0233d9366d2b3c5dace577b9f8
Mo2024 <mohdosama2025@gmail.com>
2023-05-20 04:34:18 +0300

    updated database

--- GeoHub.sql
+++ GeoHub.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 17, 2023 at 03:12 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2023 at 03:33 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `GeoHub`
+-- Database: `geohub`
 --
 
 -- --------------------------------------------------------
@@ -33,13 +33,23 @@ CREATE TABLE `choices` (
   `questionId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`choiceId`, `choice`, `questionId`) VALUES
+(21, '1', 42),
+(22, '2', 42),
+(23, '3', 42),
+(24, '4', 42);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Questions`
+-- Table structure for table `questions`
 --
 
-CREATE TABLE `Questions` (
+CREATE TABLE `questions` (
   `questionId` int(11) NOT NULL,
   `quizId` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
@@ -48,28 +58,45 @@ CREATE TABLE `Questions` (
   `answer` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`questionId`, `quizId`, `type`, `score`, `question`, `answer`) VALUES
+(41, 24, 'FITB', 4, 'what is bahrain', 'twoo sdlkjnff'),
+(42, 24, 'MCQ', 4, 'sadsa', '3'),
+(43, 24, 'TF', 4, 'ewdqdqww', 'true');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Quiz`
+-- Table structure for table `quiz`
 --
 
-CREATE TABLE `Quiz` (
+CREATE TABLE `quiz` (
   `quizid` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `nQuestions` int(11) NOT NULL,
   `boxColor` varchar(255) NOT NULL,
-  `totalTime` int(11) NOT NULL
+  `totalTime` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`quizid`, `title`, `description`, `nQuestions`, `boxColor`, `totalTime`, `uid`) VALUES
+(24, 'Middle east quiz', 'Middle east quiz lolz\r\n', 3, '#deabab', 300, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Results`
+-- Table structure for table `results`
 --
 
-CREATE TABLE `Results` (
+CREATE TABLE `results` (
   `resultId` int(11) NOT NULL,
   `score` int(11) NOT NULL,
   `timeElapsed` int(11) NOT NULL,
@@ -95,6 +122,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `username`, `email`, `fName`, `hash`, `verified`, `vcode`, `pcode`) VALUES
+(1, 'faisal', 'aksak@gmail.com', 'asdasd', '$2y$10$jIOzK76n0kMnjDTP2TtA9uaoJkapgT3FnH.hKgsj9jjJnTmrLE7Ya', 0, '0', '0');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -106,22 +140,23 @@ ALTER TABLE `choices`
   ADD KEY `questionId` (`questionId`);
 
 --
--- Indexes for table `Questions`
+-- Indexes for table `questions`
 --
-ALTER TABLE `Questions`
+ALTER TABLE `questions`
   ADD PRIMARY KEY (`questionId`),
   ADD KEY `quizId` (`quizId`);
 
 --
--- Indexes for table `Quiz`
+-- Indexes for table `quiz`
 --
-ALTER TABLE `Quiz`
-  ADD PRIMARY KEY (`quizid`);
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quizid`),
+  ADD KEY `uid` (`uid`);
 
 --
--- Indexes for table `Results`
+-- Indexes for table `results`
 --
-ALTER TABLE `Results`
+ALTER TABLE `results`
   ADD PRIMARY KEY (`resultId`),
   ADD KEY `questionId` (`questionId`),
   ADD KEY `userId` (`userId`);
@@ -140,31 +175,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `choices`
 --
 ALTER TABLE `choices`
-  MODIFY `choiceId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `choiceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT for table `Questions`
+-- AUTO_INCREMENT for table `questions`
 --
-ALTER TABLE `Questions`
-  MODIFY `questionId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `questions`
+  MODIFY `questionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
--- AUTO_INCREMENT for table `Quiz`
+-- AUTO_INCREMENT for table `quiz`
 --
-ALTER TABLE `Quiz`
-  MODIFY `quizid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `quiz`
+  MODIFY `quizid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT for table `Results`
+-- AUTO_INCREMENT for table `results`
 --
-ALTER TABLE `Results`
+ALTER TABLE `results`
   MODIFY `resultId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -174,20 +209,26 @@ ALTER TABLE `users`
 -- Constraints for table `choices`
 --
 ALTER TABLE `choices`
-  ADD CONSTRAINT `choices_ibfk_1` FOREIGN KEY (`questionId`) REFERENCES `Questions` (`questionId`);
+  ADD CONSTRAINT `choices_ibfk_1` FOREIGN KEY (`questionId`) REFERENCES `questions` (`questionId`);
 
 --
--- Constraints for table `Questions`
+-- Constraints for table `questions`
 --
-ALTER TABLE `Questions`
-  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quizId`) REFERENCES `Quiz` (`quizid`);
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quizId`) REFERENCES `quiz` (`quizid`);
 
 --
--- Constraints for table `Results`
+-- Constraints for table `quiz`
 --
-ALTER TABLE `Results`
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+
+--
+-- Constraints for table `results`
+--
+ALTER TABLE `results`
   ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`uid`),
-  ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`questionId`) REFERENCES `Questions` (`questionId`);
+  ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`questionId`) REFERENCES `questions` (`questionId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
