953b1123efaba919b9b70accc7fe9efa56a9ff76
Mo2024 <mohdosama2025@gmail.com>
2023-05-21 01:22:50 +0300

    fixed responsive

--- quiz/quizzesDisplay.php
+++ quiz/quizzesDisplay.php
@@ -6,16 +6,16 @@
         <div class="card mb-3 mt-3">
             <div class="row">
                 <div class="col-md-3">
-                    <img width="100%" height="100%" src="https://cdn.shopify.com/s/files/1/0566/6586/6400/products/6_6e504cd6-25a9-46b2-b94b-5d87768d3306_300x@2x.jpg?v=1629388839" alt="" class="img-fluid">
+                    <img src="https://cdn.shopify.com/s/files/1/0566/6586/6400/products/6_6e504cd6-25a9-46b2-b94b-5d87768d3306_300x@2x.jpg?v=1629388839" alt="" class="img-fluid h-100 w-100">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h3 class="card-title"><?php echo $quizzes[$i]['title'];?></h3>
-                        <h5 class="card-title">Created By: <?php echo $quizzes[$i]['username'];?></h5>
-                        <p class="card-text">Description: <?php echo $quizzes[$i]['description'];?></p>
-                        <p class="card-text">
-                            <small class="text-muted">Number of Questions: <?php echo $quizzes[$i]['nQuestions'];?></small>
-                        </p>
+                        <h4 class="card-title">Created By: <?php echo $quizzes[$i]['username'];?></h4>
+                        <br/>
+                        <h5 class="card-title">Number of Questions: <?php echo $quizzes[$i]['nQuestions'];?></h5>
+                        <h5 class="card-title">Description:</h5>
+                        <p class="card-text"><?php echo $quizzes[$i]['description'];?></p>
                         
                         <a href="" class="btn  btn-primary">Start Quiz</a>
                     </div>
