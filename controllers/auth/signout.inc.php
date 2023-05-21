968bc7d7a1a1c822c6077a4f00e2ed382c792ac6
Mo2024 <mohdosama2025@gmail.com>
2023-05-21 13:29:46 +0300

    fixed sign out notification

--- controllers/auth/signout.inc.php
+++ controllers/auth/signout.inc.php
@@ -2,6 +2,8 @@
 setcookie("session", password_hash($_SESSION["username"], PASSWORD_DEFAULT),time() - 3600 ,'/');
 session_start();
 session_destroy();
-header("Location: /ITCS333-Project/mainpage.php?Signout=success");
+session_start();
+$_SESSION['success'] = 'Sign out Successful';
+header("Location: /ITCS333-Project/mainpage.php");
 exit();
 ?>
