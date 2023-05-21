69b61e6f74ea6645b13cc88392841e9b54511cc3
Mo2024 <mohdosama2025@gmail.com>
2023-05-21 14:51:59 +0300

    update profile page works

--- controllers/profile/profile.inc.php
+++ controllers/profile/profile.inc.php
@@ -5,59 +5,69 @@ require("../partials/regex.inc.php");
 if(isset($_SESSION['userId']) && !empty($_SESSION['userId'])){
     $id = $_SESSION['userId'];
     $row = selectUser($id, $db);
+    if(!isset($email)){
+        $email = $row['email'];
+        $username = $row['username'];
+        $fullname = $row['fName'];
+        $redirect = true;
+    }
     
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
+    if (isset($_POST['submit'])){
         $username = $_POST['username'];
         $email = $_POST['email'];
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $birth = $_POST['birth'];
-        $pnumber = $_POST['pnum'];
+        $fullname = $_POST['fullname'];
+        $redirect = true;
 
-        if(preg_match($usernameReg, $username) && preg_match($emailReg, $email)
-        && preg_match($nameReg, $firstname) && preg_match($nameReg, $lastname)
-        && preg_match($pnumberReg, $pnumber) && preg_match($dateReg, $birth)){
+        if(!preg_match($emailReg,$email)){
+            $_SESSION['error'] = "Please make sure that the entered email is valid";
+            header("Location: /ITCS333-Project/profile/profile.php");   
+        } else if(!preg_match($usernameReg,$username)){
+            $_SESSION['error'] = "Please make sure that Username is 4 to 20 characters long, only contains alphabet letters and 0 to 9 numerics";
+            header("Location: /ITCS333-Project/profile/profile.php");   
+        } else if(!preg_match($nameReg,$fullname)){
+            $_SESSION['error'] = "Please make sure that the entered full name is entered properly";
+            header("Location: /ITCS333-Project/profile/profile.php");           
+        } else{
 
-            $usernameQuery = "SELECT * FROM user WHERE username = '$username'";
-            $emailQuery = "SELECT * FROM user WHERE email = '$email'";
+            $usernameQuery = "SELECT * FROM users WHERE username = '$username'";
+            $emailQuery = "SELECT * FROM users WHERE email = '$email'";
     
             $usernameResult = ($db->query($usernameQuery)->rowCount());
             $emailResult = ($db->query($emailQuery)->rowCount());
     
             //Cecks if username or email already exist, else it inserts the values into the database
             if($usernameResult>0 && $username != $row['username']){
-                echoAlertDanger("Username already exists");
+                $_SESSION['error'] = 'Username already exists';
+                header("Location: /ITCS333-Project/profile/profile.php");   
             }
             else if($emailResult>0 && $email != $row['email']){
-                echoAlertDanger("Email already exists");
+                $_SESSION['error'] = 'Email already exists';
+                header("Location: /ITCS333-Project/profile/profile.php");   
             }
             else{
-                $verificationStatus = $row['verified'];                
-                if ($row['email'] != $email){
-                    $verificationStatus = false;
-                }
-                $updateQuery = "UPDATE user SET username = '$username', email = '$email', firstname = '$firstname',
-                lastname = '$lastname', birthdate = '$birth', phonenumber = '$pnumber',  verified = '$verificationStatus' WHERE id = '$id'";
-                $result = $db->query($updateQuery);
+                // $verificationStatus = $row['verified'];                
+                // if ($row['email'] != $email){
+                //     $verificationStatus = false;
+                // }
+                $updateQuery = "UPDATE users SET username = :username, email = :email, fName = :fName WHERE uid = :uid";
+                $stmt = $db->prepare($updateQuery);
+                
+                $stmt->bindParam(':username', $username);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':fName', $fullname);
+                $stmt->bindParam(':uid', $id);
+                $stmt->execute();
 
                 $row = selectUser($_SESSION['userId'], $db);
-                echoAlertSuccess('User profile updated');
+                $_SESSION['success'] = 'User profile updated';
+                header("Location: /ITCS333-Project/profile/profile.php"); 
             }
-        }else{
-            echoAlertDanger(
-                "Please make sure that:
-                <ul>
-                    <li>Username is 4 to 20 characters long, only contains alphabet letters and 0 to 9 numerics </li>
-                    <li>Email, name and birthdate are enetered properly</li>
-                    <li>Password has one special character, one small letter, one capital letter and at least 8 characters long</li>
-                </ul>"
-            );
         }
-
     } 
     if(isset($_GET['verification']) && $_GET['verification'] == 'sent' && $_SERVER["REQUEST_METHOD"] == "GET"){
         if(is_null($row['verificationCode']) && $row['verified']){
-            echoAlertSuccess("Email verified");
+            $_SESSION['success'] = 'Email verified';
+            header("Location: /ITCS333-Project/profile/profile.php"); 
         }
         else{
             require('../functions/phpmailer.inc.php');
--- profile/profile.php
+++ profile/profile.php
@@ -1,57 +1,36 @@
 <?php $title = "Profile"; require('../partials/boilerplate.inc.php')?>
 <?php require('../controllers/profile/profile.inc.php')?>
-<div class="container d-flex justify-content-center align-items-center my-5">
-    <div class="row">
-        <div class="col-xl-12">
-            <div class="bg-color border card border-color-form shadow">
-                <div class="card-body">
-                    <h5 class="card-title text-secondary">Edit Profile </h5>
-                    <form class="validated-form" method="POST" novalidate>
-                        <?php if(!$row['verified']){ ?>
-                        <div class="row">
-                            <div class="mb-3 col-6">
-                                <a href="/ITCS333-Project/profile/profile.php?verification=sent">Click to verify your account</a>
-                                <?php if(isset($_GET['verification']) && $_GET['verification'] == 'sent') echo 'Check you email';?>
-                            </div>
-                        </div>
-                        <?php } ?>
-                        <div class="row">
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="email">Email</label>
-                                <input class="form-control" placeholder="Email" type="email" name="email" id="email" value="<?php echo $row['email'] ?>" required autofocus>
-                            </div>
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="username">Username</label>
-                                <input class="form-control" placeholder="Username" type="text" name="username" id="username" value="<?php echo $row['username'] ?>" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="firstname">First Name</label>
-                                <input class="form-control" placeholder="First Name" type="text" name="firstname" id="firstname" value="<?php echo $row['firstname'] ?>" required>
-                            </div>
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="lastname">Last Name</label>
-                                <input class="form-control" placeholder="Last Name" type="text" name="lastname" id="lastname" value="<?php echo $row['lastname'] ?>" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="birth">Birthdate</label>
-                                <input class="form-control" type="date" name="birth" id="birth" value="<?php echo $row['birthdate'] ?>" required>
-                            </div>
-                            <div class="mb-3 col-6">
-                                <label class="form-label" for="pnum">Phone Number</label>
-                                <input class="form-control" type="tel" placeholder="Phone Number" name="pnum" id="pnum" value="<?php echo $row['phonenumber'] ?>" required>
-                            </div>
-                        </div>
-            
-         
-                        <button type="submit" class="btn btn-success btn-block" name="submit">Submit</button>
-                    </form>
+<div class="container w-50 my-3">
+      <ul class="nav nav-underline">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">My Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="changePassword.php">Change Password</a>
+        </li>
+      </ul>
+
+      <div class="container bg-color border card border-color-form shadow p-3 justify-content-center">
+        <form class="validated-form" method="POST" novalidate">
+            <div class="row">
+                <div class="mb-3 col-sm-6">
+                    <label class="form-label" for="fullname">Full Name</label>
+                    <input class="form-control" placeholder="Full Name" type="text" name="fullname" id="fullname" value="<?php if($redirect) echo $fullname?>" >
+                </div>
+                <div class="mb-3 col-sm-6">
+                    <label class="form-label" for="InputFullName">Username</label>
+                    <input class="form-control" placeholder="Username" type="text" name="username" id="username" value="<?php if($redirect) echo $username?>" required>
                 </div>
             </div>
-        </div>
-    </div>
+            <div class="row">
+                <div class="mb-3 col-sm-6">
+                    <label class="form-label" for="email">Email</label>
+                    <input class="form-control" placeholder="Email" type="email" name="email" id="email" value="<?php if($redirect) echo $email?>" required>
+                </div>
+            </div>
+            <a class="btn btn-secondary mb-2">Cancel</a>
+            <button name="submit" type="submit" class="btn btn-primary mb-2">Save Changes</button>
+        </form>
+      </div>
 </div>
 <?php require('../partials/footer.inc.php')?>
